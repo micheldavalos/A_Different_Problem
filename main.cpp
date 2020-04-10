df6fb67515a4d33788c943196506cf69c5d21239
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-04-09 22:28:19 -0500

    a la primera

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	unsigned long long a, b;
+
+	while (cin >> a >> b) {
+		if (a > b) {
+			cout << a - b << endl;
+		}
+		else {
+			cout << b - a << endl;
+		}
+	}
+	return 0;
+}
